29c1e7def153a4ca6c91a3d2d20bf622d2a75f82
wmason1997 <williamcmason1997@gmail.com>
2024-03-17 23:24:04 -0700

    Edited App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { setContext } from '@apollo/client/link/context';
 import { Provider } from 'react-redux';
 import store from './utils/store';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+//import { StoreProvider } from './utils/GlobalState';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
