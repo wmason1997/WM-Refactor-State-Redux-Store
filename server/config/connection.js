ef384bbca1021ab7662e10ebfcc7c6474595a932
wmason1997 <williamcmason1997@gmail.com>
2024-03-20 20:23:08 -0700

    added string because dotenv is broken

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect('mongodb+srv://williamcmason1997:Ilikeextracredit100!@cluster0.r3t9zgb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
